32ebaf6f9ff323c1a4df2e051575fe0e6f0812a1
abdullacaad <mabdullahsaadofficial@gmail.com>
2025-02-15 14:03:11 +0500

    Add files via upload

--- TASK1.py
+++ TASK1.py
@@ -1,7 +1,12 @@
 name="Alice"
 age=19
 is_student=True
+dec=1.5
 
 print("Name:",name)
 print("Age:",age)
 print("Student",is_student)
+print(type(age))
+print(int(dec))
+print(name[:3])
+print(name[-3:])
